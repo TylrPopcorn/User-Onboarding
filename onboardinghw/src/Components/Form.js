cf12e06ad79a6a2767f947d21a36b2a63a04ce27
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-06-23 21:08:54 -0400

    Finished

---
+++ onboardinghw/src/Components/Form.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const Form = function (props) {
+    const { change, submit, errors } = props;
+    const { username, email, password, tos } = props.values;
+
+    const onChange = (e) => {
+        const { name, value, checked, type } = e.target;
+        const newVal = type === "checkbox" ? checked : value;
+        change(name, newVal)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        submit();
+    }
+
+    return (
+        <div>
+            <h1> My Form </h1>
+            <p>{errors.username}</p>
+            <p>{errors.password}</p>
+
+            <form onSubmit={onSubmit}>
+                <label> Name:
+                    <input
+                        type="text"
+                        value={"username here"}
+                        name="username"
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label> Email:
+                    <input
+                        name="email"
+                        type="email"
+                        value={"email here"}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label> Password:
+                    <input
+                        name="password"
+                        type="password"
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label> Terms of Service:
+                    <input
+                        type="checkbox"
+                        name="tos"
+                        checked={tos}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <input type="submit" value={"Create"} />
+
+            </form>
+        </div>
+    )
+}
+
+export default Form;
